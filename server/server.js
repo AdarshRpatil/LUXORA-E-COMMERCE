9147373769ca499b8eb0ec7bd0c2d31fe0a51350
AdarshRpatil <adarshrpatil02@gmail.com>
2025-01-14 22:13:50 +0530

    url change

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     // origin: [process.env.CLIENT_BASE_URL, process.env.CLIENT_BASE_URL_RENDER],
-    origin: [process.env.CLIENT_BASE_URL_RENDER],
+    // origin: [process.env.CLIENT_BASE_URL_RENDER],
+    origin: [process.env.CLIENT_BASE_URL],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
